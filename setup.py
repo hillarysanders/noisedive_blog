33c7e9093cdfcc742897eb2447b5e061f6021be7
Hillary Sanders <hillary.sanders@opendoor.com>
2023-04-24 21:01:34 -0700

    1.1.7

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 
 from setuptools import setup, find_packages
 
+# Read the requirements.txt file and store the dependencies in a list
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+
 setup(
     #this will be the package name you will see, e.g. the output of 'conda list' in anaconda prompt
     name = 'noisedive_flask', 
     #some version number you may wish to add - increment this after every update
-    version='1.1.6', 
+    version='1.1.7', 
     include_package_data=True,
-    # Use one of the below approach to define package and/or module names:
-  
-    #if there are only handful of modules placed in root directory, and no packages/directories exist then can use below syntax
-#     packages=[''], #have to import modules directly in code after installing this wheel, like import mod2 (respective file name in this case is mod2.py) - no direct use of distribution name while importing
-  
-    #can list down each package names - no need to keep __init__.py under packages / directories
-#     packages=['<list of name of packages>'], #importing is like: from package1 import mod2, or import package1.mod2 as m2
-  
+
+    install_requires=requirements,
+
     #this approach automatically finds out all directories (packages) - those must contain a file named __init__.py (can be empty)
     packages=find_packages(), #include/exclude arguments take * as wildcard, . for any sub-package names
 )
@@ -61,5 +61,6 @@ setup(
 # wrote daemon to run /home/protected/noisedive/venv/bin/waitress-serve --call 'noisedive_flask:create_app'
 # kicked it off in nearlyfreespeech
 # logs of daemon are in: /home/logs/daemon_noisedive.log
+# changed setup.py to have include_package_data=True so non-py files are included in package.
 
 # exec venv/bin/waitress-serve --call 'noisedive_flask:create_app'
