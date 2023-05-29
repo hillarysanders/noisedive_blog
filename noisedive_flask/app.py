cbcac27a58ef2160a01d56b1120efc919826e832
Hillary Sanders <hillary.sanders@opendoor.com>
2023-05-29 16:06:01 -0700

    debug

--- noisedive_flask/app.py
+++ noisedive_flask/app.py
@@ -109,4 +109,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host=socket.gethostbyname(socket.gethostname()))
+    app.run(debug=False, host=socket.gethostbyname(socket.gethostname()))
